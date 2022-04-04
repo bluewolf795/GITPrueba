6dc58999dd9aa39bf07be9b994bba45a00af35a3
bluewolf795 <102964127+bluewolf795@users.noreply.github.com>
2022-04-04 14:15:57 +0200

    Update notas.java
    Javadoc comentarios eliminados.

--- notas.java
+++ notas.java
@@ -1,30 +1,16 @@
 package entornosactividad2;
 import java.util.Scanner;
 
-/**
- * Clase notas en la que declaramos las variables y creamos un escaner que luego necesitaremos
- * @author Germán
- */
 public class notas {
-    /**
-     * uf1 = la nota de la primera unidad
-     * uf2 = la nota de la segunda unidad
-     * uf3 = la nota de la tercera unidad
-     */
+
     double uf1, uf2, uf3;
-    /**
-     * ac1 = se utilizará para crear la ponderación de la primera unidad en un método más adelante
-     * ac2 = se utilizará para crear la ponderación de la primera unidad en un método más adelante
-     * ac3 = se utilizará para crear la ponderación de la primera unidad en un método más adelante
-     */
+
     double acu1, acu2, acu3, def;
     //utilizames escaner para poder introducir datos
     Scanner entrada = new Scanner(System.in);
     
     
-/**
- * Función que muestra un mensaje por pantalla para dejar saber al usuario cuando debe introducir las notas de la uf1, 2 y 3. * 
- */
+
     public void IngresaNotas() {
         //cuando ejeuctamos este metodo lo primnero que queremos es que nos pida notas
         System.out.println("ingrese las notas del estudiante");
@@ -46,9 +32,7 @@ public class notas {
     }
     
     // metodo para comprobar bien entroduccion de notas
-    /**
-     * Método para asegurar que la introducción de datos sea correcta, impidiendo que se escriba un número superior al 10, sin embargo permita que sea inferior a 0.
-     */
+
     public void comprobacion(){
         
         if (uf1>10) {
@@ -76,9 +60,7 @@ public class notas {
         
     }
     // metodo para calcular nota
-    /**
-     * Este método calcula las notas pero no las muestra, pondera las 3 unidades formativas y calcula la nota definitiva
-     */
+
     public void Calculonotas() {
         acu1 = uf1 * 0.35;
         acu2 = uf2 * 0.35;
@@ -89,9 +71,7 @@ public class notas {
         //hasta aqui la tenemos calculada pero no la mostramos
     }
     
-    /**
-     * Esta función muestra las notas por pantalla, todas las unidades formativas y la definitiva con las ponderaciones
-     */
+
     public void Mostrar() {
         //print ln lo que hace es que al terminar el print baja la linea
         System.out.println(" notas introducidas son:");
@@ -106,9 +86,7 @@ public class notas {
         System.out.println(" nota definitiva es = "+ def);
         
     }
-    /**
-     * En este método no aparece la nota, si no que se muestra por pantalla si el alumno ha aprobado o suspendido, o da error en caso de que el número sea erróneo.
-     */
+
     public void aprobado() {
             
             if(def<5 && def>=0) {
@@ -122,9 +100,7 @@ public class notas {
             }
         }
         
-    /**
-     * Finalmente tenemos las funciones juntas en el main para que se ejecuten una tras la otra.
-     */    
+
     public static void main(String[] args) {
         // creamos mecanimos para llamar a cualquier metodo fuero de la clase
         notas fc= new notas();
